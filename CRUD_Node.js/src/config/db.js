8c7a4c6ab322d8ea70fd193dac78b2432e7daf8c
alejandro-dab <alejandro_ab24@hotmail.com>
2025-11-24 13:40:21 -0600

    Ajuste de puertos para Railway

--- CRUD_Node.js/src/config/db.js
+++ CRUD_Node.js/src/config/db.js
@@ -1,16 +1,13 @@
-//Driver mysql2 para tener compatibilidad con el MYSQL actual
-const mysql = require('mysql2'); //Importación de MYSQL
-require('dotenv').config(); 
+const mysql = require('mysql2');
+require('dotenv').config();
 
 const conexion = mysql.createConnection({
-    // Node busca la variable. Si está en Railway, la usa. 
-    // Si no, la busca en el archivo .env local.
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    //Password de Railway o local
+    password: process.env.DB_PASS || process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    // Puerto general de Rwailway
-    port: process.env.DB_PORT || 3306 
+    port: process.env.DB_PORT || 3306
 });
 
 conexion.connect((error) => {
@@ -18,7 +15,7 @@ conexion.connect((error) => {
         console.log("Error de conexión: " + error);
         return;
     }
-    console.log("¡Conectado a la Base de Datos 'Suministros' exitosamente!");
+    console.log("¡Conectado a la Base de Datos exitosamente!");
 });
 
 module.exports = conexion;
